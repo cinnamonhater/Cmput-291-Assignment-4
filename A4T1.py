ac3a08a10f80dd5f0605344be0e5336d0cb60c9b
Samuel Habtu <shabtu@cs.ualberta.ca>
2021-11-30 15:50:20 -0700

    task one-> duplicate key error

--- A4T1.py
+++ A4T1.py
@@ -1,9 +1,27 @@
 from pymongo import MongoClient
 
+
+def dropCOllections(database, names):
+
+    for name in names:
+        database.drop_collection(name[:-5])
+
+def getData(name):
+
+    with open(name) as file:
+        print("FIle opened? :O")
+        data = file.read()
+    data = data.replace("$oid", "ObjectId")
+    file.close()
+    return eval(data)
+
 def main():
     file_names = ["artists.json", "tracks.json"]
-    # Use client = MongoClient('mongodb://localhost:27017') for specific ports!
-    client = MongoClient('mongodb://localhost:27017')
+    #https://docs.mongodb.com/manual/administration/install-community/
+    #USE VERSION 3.7.0 NOT 4 of mongodb
+    #https://pymongo.readthedocs.io/en/stable/tutorial.html
+
+    client = MongoClient('mongodb://localhost:27012')
     db_name = "A4dbNorm"
     database = client[db_name]
     artists_collection = database[file_names[0][:-5]]
@@ -11,9 +29,14 @@ def main():
     collections = database.list_collection_names()
     for name in (file_names):
         if(name[:-5] in collections):
-            print(f"collection '{name}' exists")
+            print(f"collection already '{name[:-5]}' exists")
 
-    print(collections)
+    artists = getData(file_names[0])
+    tracks  = getData(file_names[1])
+    database = dropCOllections(database, file_names)
+
+    
+
 
 if __name__ == "__main__":
     main()
