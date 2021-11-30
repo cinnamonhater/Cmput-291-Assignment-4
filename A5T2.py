ffd69148209755e11e51829048dd02904dd34d09
Samuel Habtu <shabtu@cs.ualberta.ca>
2021-11-30 14:32:55 -0700

    Timeout???

---
+++ A5T2.py
@@ -0,0 +1,19 @@
+from pymongo import MongoClient
+
+def main():
+    file_names = ["artists.json", "tracks.json"]
+    # Use client = MongoClient('mongodb://localhost:27017') for specific ports!
+    client = MongoClient('mongodb://localhost:27017')
+    db_name = "A4dbNorm"
+    database = client[db_name]
+    artists_collection = database[file_names[0][:-5]]
+    tracks_collection  = database[file_names[1][:-5]]
+    collections = database.list_collection_names()
+    for name in (file_names):
+        if(name[:-5] in collections):
+            print(f"collection '{name}' exists")
+    
+    print(collections)
+
+if __name__ == "__main__":
+    main()
