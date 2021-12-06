fdb2fa86b73fdfcad265a6e474863f0fe169035b
cinnamonhater <89957213+cinnamonhater@users.noreply.github.com>
2021-12-05 22:06:22 -0700

    Update A4Q2Embed.py
    Adding python code to take queries

--- A4Q2Embed.py
+++ A4Q2Embed.py
@@ -1,4 +1,10 @@
 def main():
+    import pymongo
+    myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+    mydb = myclient["A4dbEmbed"]
+    mycol = mydb[tracks]
+    myquery = {//query in here}
+    mydoc = mycol.find(myquery)
     pass
 
 if __name__ == "__main__":
